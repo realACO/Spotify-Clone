1857d9b46b8150973ae41e0490bd88a9cfeb3be4
realACO <ashishoomman@gmail.com>
2026-03-06 13:16:11 +0530

    file directory fixed

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const path = require("path");
 const authRoutes = require("./routes/auth.routes");
 const musicRoutes = require("./routes/music.routes");
 
@@ -9,5 +10,12 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/music", musicRoutes);
+
+const frontendDistPath = path.resolve(__dirname, "../../frontend/dist");
+app.use(express.static(frontendDistPath));
+
+app.get(/^(?!\/api).*/, (req, res) => {
+  res.sendFile(path.join(frontendDistPath, "index.html"));
+});
 
 module.exports = app;
